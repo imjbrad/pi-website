7ad4142f277571dd383105b70ad39b752da1aecf
Jordan Bradley <jbradley@mica.edu>
2016-06-18 18:31:46 -0400

    lots of progress

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,4 +1,41 @@
-export function runBlock ($log) {
+export function runBlock ($timeout, $rootScope) {
   'ngInject';
-  $log.debug('runBlock end');
+
+  var scrollTimer;
+
+  //Scroll helper
+  angular.element(window).scroll(function() {
+
+    var scrollTop = $(this).scrollTop();
+
+    //$rootScope.scrollingNearTop = (scrollTop < 300);
+
+    $rootScope.$apply(function(){
+      $rootScope.scrolling = true;
+    });
+
+    $timeout.cancel(scrollTimer);
+
+    scrollTimer = $timeout(function() {
+      $rootScope.scrolling = false;
+      console.log($rootScope.scrolling);
+    }, 250);
+
+  });
+
+  var menuBlock = $(".short-cuts");
+  $rootScope.scrollingNearTop = true;
+
+  new Waypoint({
+    element: menuBlock[0],
+    handler: function() {
+      $rootScope.scrollingNearTop = !$rootScope.scrollingNearTop;
+      $rootScope.$apply();
+      console.log("passed");
+    }
+    //offset: function() {
+    //  return -this.element.clientHeight
+    //}
+  })
+
 }
