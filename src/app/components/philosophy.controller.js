6aba72220cb93db1caf98219c9d949002e8a0ed3
Jordan Bradley <jbradley@mica.edu>
2016-08-23 21:34:05 -0400

    Various copy changes

--- src/app/components/philosophy.controller.js
+++ src/app/components/philosophy.controller.js
@@ -4,15 +4,15 @@ export function PhilosophyCtrl($scope){
   $scope.stats = [
     {
       title: 'Students have trouble sleeping.',
-      details: '73% of students report sleep problems. When wellness suffers, productivity is bound to suffer as a result. By keeping wellness indicators at the forefront, Pi aims to ensure student success.'
+      details: '73% of students report sleep problems. When wellness suffers, productivity is bound to suffer as a result. By keeping wellness indicators at the forefront of our daily planning app, Pi aims to prevent burnout and encourage balance.'
     },
     {
       title: "Achieving balance is tough for students.",
-      details: "The problem doesn't stop when class is dismissed. As 70% of college students work, $27b was lost in 2015 to mental health related work absences. Additionally, 3/6 of the top reasons melinials are unhappy at work have to do with work life balance."
+      details: "$27b was lost in 2015 to mental-health-related absences from work. Considering that fact that 70% of college students work, and 3/6 of the top reasons millennials are unhappy at work have to do with work-life balance, it is clear that the problem doesn’t stop when class is dismissed."
     },
     {
-      title: 'Gen Z has big dreams.',
-      details: '72% of Gen Z wants to start businesses. Gen Z has big dreams, but we believe burn out will keep them from reaching their full potential. Pi creates solutions at the nexus of wellness, productivity, and students\' personal mission. We call this holistic productivity.'
+      title: 'Generation Z has big dreams.',
+      details: '72% of Gen Z wants to start businesses. Gen Z has big dreams, but we believe burnout will keep them from reaching their full potential. Pi creates solutions at the nexus of wellness, productivity, and students\' personal mission. We call this holistic productivity.'
     }
   ];
 
